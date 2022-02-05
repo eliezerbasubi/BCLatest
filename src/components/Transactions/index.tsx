87915a0ef0dc0f3efa3e84dcd8faaefb511bfef6
Eliezer Basubi <eliezer.basubi30@gmail.com>
2022-02-05 10:58:01 +0300

    Improve transactions table

--- src/components/Transactions/index.tsx
+++ src/components/Transactions/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import VSpinner from "../../assets/vectors/VSpinner";
 import { useBlockchain } from "../../contexts/blockchainProvider";
+import TransactionTable from "./TransactionTable";
 
 const Transactions = () => {
   const { currentBlock, loading, isWeb3Supported } = useBlockchain();
@@ -14,41 +15,42 @@ const Transactions = () => {
           Transactions - #{currentBlock.blockNumber}
         </h3>
 
-        <div className="container mx-auto">
-          <div className="py-4 w-full flex justify-center mt-8">
-            {(loading && <VSpinner />) || null}
-            {!loading && (
-              <div className="flex justify-center bg-white border rounded-2xl p-8 w-full overflow-x-auto">
-                <table className="table-auto border-0">
-                  <thead>
-                    <tr>
-                      <th className="py-3">Transaction Hash</th>
-                      <th className="py-3">Amount</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {currentBlock.transactions?.map((transaction, index) => (
-                      <tr key={index.toFixed()}>
-                        <td className="pb-3 border-0">
-                          <div className="font-medium bg-light-gray py-3 p-4 rounded-tl-md rounded-bl-md">
-                            {transaction.blockHash}
-                          </div>
-                        </td>
-                        <td className="pb-3 border-0">
-                          <div className="font-medium bg-light-gray py-3 p-4 rounded-tr-md rounded-br-md">
-                            {transaction.value}
-                          </div>
-                        </td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </table>
-              </div>
-            )}
-          </div>
+        {/* <div className="container mx-auto"> */}
+        <div className="py-4 w-full flex justify-center mt-8">
+          {(loading && <VSpinner />) || null}
+          {!loading && (
+            <TransactionTable data={currentBlock.transactions} />
+            // <div className="flex justify-center bg-white border rounded-2xl p-8 w-full overflow-x-auto">
+            //   <table className="table-auto border-0">
+            //     <thead>
+            //       <tr>
+            //         <th className="py-3">Transaction Hash</th>
+            //         <th className="py-3">Amount</th>
+            //       </tr>
+            //     </thead>
+            //     <tbody>
+            //       {currentBlock.transactions?.map((transaction, index) => (
+            //         <tr key={index.toFixed()}>
+            //           <td className="pb-3 border-0">
+            //             <div className="font-medium bg-light-gray py-3 p-4 rounded-tl-md rounded-bl-md">
+            //               {transaction.blockHash}
+            //             </div>
+            //           </td>
+            //           <td className="pb-3 border-0">
+            //             <div className="font-medium bg-light-gray py-3 p-4 rounded-tr-md rounded-br-md">
+            //               {transaction.value}
+            //             </div>
+            //           </td>
+            //         </tr>
+            //       ))}
+            //     </tbody>
+            //   </table>
+            // </div>
+          )}
         </div>
       </div>
     </div>
+    // </div>
   );
 };
 
