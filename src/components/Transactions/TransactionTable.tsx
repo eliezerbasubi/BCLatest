1076a97164dd689b8327e21ad560b7f513667765
Eliezer Basubi <eliezer.basubi30@gmail.com>
2022-02-05 11:20:05 +0300

    Rf: Improve transactions table layout

--- src/components/Transactions/TransactionTable.tsx
+++ src/components/Transactions/TransactionTable.tsx
@@ -8,18 +8,23 @@ type Props = {
 const TransactionTable = ({ data }: Props) => {
   return (
     <div className="container mx-auto">
-      <div className="w-full overflow-auto bg-white border rounded-2xl p-8 max-h-[500px]">
+      <div className="w-full overflow-auto bg-white border rounded-2xl p-8 lg:px-24 max-h-[500px]">
         <div>
           <div className="flex items-center font-bold">
-            <div className="p-3 text-left w-2/3 sm:w-1/2">Transaction Hash</div>
+            <div className="p-3 text-left w-2/3 sm:w-1/2 lg:w-2/3">
+              Transaction Hash
+            </div>
             <div className="p-3 text-left">Amount</div>
           </div>
           {data.map((txn) => (
-            <div className="flex mb-2" key={txn.transactionIndex}>
-              <div className="w-4/5 sm:w-1/2 font-medium bg-light-gray py-3 p-4 rounded-tl-md rounded-bl-md truncate">
+            <div
+              className="flex mb-2 bg-light-gray rounded-md"
+              key={txn.transactionIndex}
+            >
+              <div className="w-4/5 sm:w-1/2 lg:w-2/3 font-medium py-3 p-4 rounded-tl-md rounded-bl-md truncate">
                 {txn.blockHash}
               </div>
-              <div className="w-36 sm:w-48 font-medium bg-light-gray py-3 p-4 rounded-tr-md rounded-br-md truncate">
+              <div className="w-36 sm:w-48 font-medium py-3 p-4 rounded-tr-md rounded-br-md truncate">
                 {txn.value}
               </div>
             </div>
